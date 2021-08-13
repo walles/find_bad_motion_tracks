ee88cf0a07984b00f6b119975b3d754b183b00c6
Johan Walles <johan.walles@gmail.com>
2021-08-13 22:13:57 +0200

    Give all release files the same name
    I think this will fix the upgrading experience.

--- release.sh
+++ release.sh
@@ -13,13 +13,14 @@ echo
 echo "The tag description will be on the releases page on Github, write something nice!"
 git tag --annotate "$VERSION"
 
-# If we name it with 1.2.3 at the end Blender fails to load it, so the .py file
-# name version number must be underscore separated.
+RELEASE_DIR="releases/$VERSION"
+mkdir "$RELEASE_DIR"
+
 UNDERSCORE_VERSION=$(echo "$VERSION" | tr . _)
 COMMAS_VERSION="${UNDERSCORE_VERSION//_/, }"
-sed "s/    \"version\": .*/    \"version\": ($COMMAS_VERSION),/" <"__init__.py" >"find_bad_tracks-${UNDERSCORE_VERSION}.py"
+sed "s/    \"version\": .*/    \"version\": ($COMMAS_VERSION),/" <"__init__.py" >"$RELEASE_DIR/find_bad_tracks.py"
 
 git push --tags
 
-echo "Please upload find_bad_tracks-${UNDERSCORE_VERSION}.py at https://github.com/walles/find_bad_motion_tracks/releases/tag/$VERSION"
+echo "Please upload $RELEASE_DIR/find_bad_tracks.py at https://github.com/walles/find_bad_motion_tracks/releases/tag/$VERSION"
 read -r -p "Press RETURN when done > "
