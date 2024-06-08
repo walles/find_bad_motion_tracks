df41dd70132e8cf7a930421a11641e89f076305f
Johan Walles <johan.walles@gmail.com>
2024-06-08 19:36:41 +0200

    Extract packaging into its own file

---
+++ package-versioned-zip.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# https://sipb.mit.edu/doc/safe-shell/
+set -eu -o pipefail
+shopt -s failglob
+
+# Read version number parameter
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+VERSION=$1
+
+# Validate that the version is in 1.2.3 format
+if ! [[ "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Version number must be in 1.2.3 format: $VERSION"
+    exit 1
+fi
+
+WORKDIR=$(mktemp -d)
+trap 'rm -rf ${WORKDIR}' EXIT
+
+# Create a find-bad-tracks-1_2_3.zip file containing files with the new version
+# number in them. This is what should be uploaded to Blender's extension site.
+UNDERSCORE_VERSION=$(echo "$VERSION" | tr . _)
+ZIPNAME="${PWD}/find-bad-motion-tracks-${UNDERSCORE_VERSION}.zip"
+mkdir "${WORKDIR}/find_bad_motion_tracks"
+sed "s/^version = .*/version = \"${VERSION}\"/" <blender_manifest.toml >"${WORKDIR}/find_bad_motion_tracks/blender_manifest.toml"
+cp ./*.py "${WORKDIR}/find_bad_motion_tracks/"
+pushd "${WORKDIR}" >/dev/null
+zip -r "${ZIPNAME}" "find_bad_motion_tracks" >/dev/null
+popd >/dev/null
+
+echo "${ZIPNAME}"
--- release.sh
+++ release.sh
@@ -10,20 +10,8 @@ echo
 echo "Please enter the new version number in 1.2.3 format"
 read -r -p "> " VERSION
 
-WORKDIR=$(mktemp -d)
-trap 'rm -rf ${WORKDIR}' EXIT
-
-# Create a find-bad-tracks-1_2_3.zip file containing files with the new version
-# number in them. This is what should be uploaded to Blender's extension site.
-UNDERSCORE_VERSION=$(echo "$VERSION" | tr . _)
-COMMAS_VERSION="${UNDERSCORE_VERSION//_/, }"
-ZIPNAME="${PWD}/find-bad-motion-tracks-${UNDERSCORE_VERSION}.zip"
-mkdir "${WORKDIR}/find_bad_motion_tracks"
-sed "s/    \"version\": .*/    \"version\": ($COMMAS_VERSION),/" <"__init__.py" >"${WORKDIR}/find_bad_motion_tracks/__init__.py"
-sed "s/^version = .*/version = \"${VERSION}\"/" <blender_manifest.toml >"${WORKDIR}/find_bad_motion_tracks/blender_manifest.toml"
-pushd "${WORKDIR}" >/dev/null
-zip -r "${ZIPNAME}" "find_bad_motion_tracks" >/dev/null
-popd >/dev/null
+PACKAGED=$(./package-versioned-zip.sh "$VERSION")
+echo Packaged into: "$PACKAGED"
 
 echo
 echo "The tag description will be on the releases page on Github, write something nice!"
@@ -31,8 +19,8 @@ git tag --annotate "$VERSION"
 
 git push --tags
 
-echo "Please upload ${ZIPNAME} at https://github.com/walles/find_bad_motion_tracks/releases/tag/$VERSION"
+echo "Please upload ${PACKAGED} at https://github.com/walles/find_bad_motion_tracks/releases/tag/$VERSION"
 read -r -p "Press RETURN when done > "
 
-echo "Please upload ${ZIPNAME} at https://extensions.blender.org"
+echo "Please upload ${PACKAGED} at https://extensions.blender.org"
 read -r -p "Press RETURN when done > "
